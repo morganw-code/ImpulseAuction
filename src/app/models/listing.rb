75cca465db7bc4d1f84a537b89da19b8def27210
Morgan Webb <mnbwebb2233@gmail.com>
2020-05-13 12:46:32 +1000

    Add devise for user authentication

--- src/app/models/listing.rb
+++ src/app/models/listing.rb
@@ -1,7 +1,3 @@
 class Listing < ApplicationRecord
-    after_update :reload_page
-
-    private
-    def reload_page
-    end
+    belongs_to :user
 end
--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get "/", :to => "listings#index", :as => :root
   mount ActionCable.server => "cable"
 end
